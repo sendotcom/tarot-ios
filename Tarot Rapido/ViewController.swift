e97d09187222d77129ac7236721dcabb1d5efb70
H. Giovani Osorio <go.cervantes@gmail.com>
2022-04-08 21:08:13 -0500

    Initial Commit

---
+++ Tarot Rapido/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarot Rapido
+//
+//  Created by H.Gio on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
